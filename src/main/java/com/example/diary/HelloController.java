7327b0c37a11013851769556c6465366995fa1c1
devsh <digimon.1740@gmail.com>
2018-03-17 00:08:25 +1100

    생성자 DI

--- src/main/java/com/example/diary/HelloController.java
+++ src/main/java/com/example/diary/HelloController.java
@@ -1,26 +1,33 @@
 package com.example.diary;
 
+import com.example.diary.domain.user.User;
+import com.example.diary.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class HelloController {
 
-	@GetMapping(value="/")
+	UserService userService;
+
+	@Autowired
+	public HelloController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping(value = "/")
 	public String hello() throws Exception {
 		return "redirect:login.html";
 	}
 
-	@GetMapping(value="/login-error")
+	@GetMapping(value = "/login-error")
 	public String loginError() throws Exception {
 		return "/login-error.html";
-	}
-
-	@GetMapping(value="/main")
-	public String main() throws Exception {
-		return "redirect:layout.html";
 	}
 }
